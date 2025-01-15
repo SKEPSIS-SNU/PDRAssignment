a478c935e9173dc059b1556e2fc3f9654adf8f0e
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2025-01-15 20:47:52 +0530

    update middleware to activate task deadline

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/uploadthing",
+  "/api/daily/cron",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
