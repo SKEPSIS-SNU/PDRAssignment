883d626bb6495254c21b473a29b03168d5e54519
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2025-01-20 00:28:25 +0530

    logo color change

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -7,14 +7,24 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <header className="flex justify-between items-center gap-4 flex-row flex-wrap py-6">
-      <Image
-        priority={true}
-        src="/logo.png"
-        width={100}
-        height={100}
-        alt="logo"
-        className="invert dark:invert-0"
-      />
+      <div>
+        <Image
+          priority={true}
+          src="/logo_light.png"
+          width={100}
+          height={100}
+          alt="logo"
+          className="dark:hidden"
+        />
+        <Image
+          priority={true}
+          src="/logo_dark.png"
+          width={100}
+          height={100}
+          alt="logo"
+          className="hidden dark:inline-block"
+        />
+      </div>
 
       <div className="flex flex-row flex-wrap items-center gap-2">
         <ModeToggle />
