54c3b0ea4c07a3714dfad72624ff9eb061eaff81
Shuvadeep Mandal <shuvadeepmandal5@gmail.com>
2025-01-20 00:13:41 +0530

    Logo image optimization

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -7,7 +7,14 @@ import Image from "next/image";
 const Navbar = () => {
   return (
     <header className="flex justify-between items-center gap-4 flex-row flex-wrap py-6">
-      <Image src="/logo.png" width={100} height={100} alt="logo" className="invert dark:invert-0" />
+      <Image
+        priority={true}
+        src="/logo.png"
+        width={100}
+        height={100}
+        alt="logo"
+        className="invert dark:invert-0"
+      />
 
       <div className="flex flex-row flex-wrap items-center gap-2">
         <ModeToggle />
